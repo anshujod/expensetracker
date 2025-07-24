c63452eec72e8f40b308da9023cd222688f1b0ee
Anshu Prakash <anshuprakash55@gmail.com>
2025-07-24 16:42:28 +0530

    final changes made for the deployment

--- backend/server.js
+++ backend/server.js
@@ -31,5 +31,5 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
